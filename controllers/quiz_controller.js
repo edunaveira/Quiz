7d3eacebff3ca44f2c79556887ddea6283d9925e
edu <edurapp@gmail.com>
2015-07-15 05:11:14 +0200

    Primera Pregunta

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,19 @@
+//GET /quizes/question
+exports.question  = function(req, res){
+	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+};
+
+//GET /quizes/answer
+exports.answer = function(req, res){
+
+	var respuesta = req.query.respuesta;
+
+    var patt = new RegExp(/roma/i);
+    var validacion = patt.test(respuesta);
+
+	if(validacion){
+		res.render('quizes/answer', {respuesta: "Correcto"});
+	}else{
+		res.render('quizes/answer', {respuesta: "Incorrecto"});
+	}
+};
